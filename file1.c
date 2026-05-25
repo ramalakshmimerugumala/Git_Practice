589f80a61c37c14fb2b942c7b7bb04c2a25a8f7c
Ramalakshmi <merugumala.ramalakshmi@gmail.com>
2026-05-25 19:44:51 +0530

    last

--- file1.c
+++ file1.c
@@ -6,8 +6,13 @@ int main(){
  printf("git diff command");
  printf("Adding");
  printf("checking git branch working");
+<<<<<<< HEAD
  printf("Working on Pull command");
  printf("Ckecking on git Fetch");
  printf("working on Git Stash");
  printf("Working on reset\n");
+=======
+ printf("Cherry pick code");
+>>>>>>> 10086d5 (cherry pick commit)
+printf("Stash testing");
 }

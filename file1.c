dc0a767b82c6e1890beb0e0a3edfbcaf4996b6ce
Rama Lakshmi.Merugumala@31 <143600595+ramalakshmimerugumala@users.noreply.github.com>
2026-06-03 22:49:45 +0530

    Update file1.c

--- file1.c
+++ file1.c
@@ -6,14 +6,14 @@ int main(){
  printf("git diff command");
  printf("Adding");
  printf("checking git branch working");
-<<<<<<< HEAD
+//<<<<<<< HEAD
  printf("Working on Pull command");
  printf("Ckecking on git Fetch");
  printf("working on Git Stash");
  printf("Working on reset\n");
-=======
+//=======
  printf("Cherry pick code");
->>>>>>> 10086d5 (cherry pick commit)
+//>>>>>>> 10086d5 (cherry pick commit)
 printf("Stash test");
 printf("Stash");
 printf("Working on git Poll SCM");

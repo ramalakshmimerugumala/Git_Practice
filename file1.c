c94d8e77e81970d3538fe3baa5ea758723d61788
Rama Lakshmi.Merugumala@31 <143600595+ramalakshmimerugumala@users.noreply.github.com>
2026-06-03 22:59:02 +0530

    Refactor print statements for consistency

--- file1.c
+++ file1.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 int main(){
  printf("Hello World\n");
- printf("Learning Git");
- printf("Second Commit");
- printf("git diff command");
- printf("Adding");
- printf("checking git branch working");
+ printf("Learning Git\n");
+ printf("Second Commit\n");
+ printf("git diff command\n");
+ printf("Adding\n");
+ printf("checking git branch working\n");
 //<<<<<<< HEAD
- printf("Working on Pull command");
- printf("Ckecking on git Fetch");
- printf("working on Git Stash");
+ printf("Working on Pull command\n");
+ printf("Ckecking on git Fetch\n");
+ printf("working on Git Stash\n");
  printf("Working on reset\n");
 //=======
- printf("Cherry pick code");
+ printf("Cherry pick code\n");
 //>>>>>>> 10086d5 (cherry pick commit)
-printf("Stash test");
-printf("Stash");
-printf("Working on git Poll SCM");
-printf("Working on the jenkin webhook");
-printf("Working on Pipeline");
+printf("Stash test\n");
+printf("Stash\n");
+printf("Working on git Poll SCM\n");
+printf("Working on the jenkin webhook\n");
+printf("Working on Pipeline\n");
 printf("CI/CD test\n");
 
 }
